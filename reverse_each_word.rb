b3a105c80d4593935cb74ab9354ee4b2eafc9f30
Paul Ralyea <047par@gmail.com>
2020-03-20 02:59:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
   str
     .scan(/(\s*)(\S+)(\s*)/)
-    .each { |spacer1, word, spacer2| spacer1 + word.reverse + spacer2 }
+    .map { |spacer1, word, spacer2| spacer1 + word.reverse + spacer2 }
     .join
 end
   
