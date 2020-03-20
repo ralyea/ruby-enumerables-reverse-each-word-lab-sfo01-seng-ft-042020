e83a0f9af61529d0a33344cca1adaa3b23f3f1d8
Paul Ralyea <047par@gmail.com>
2020-03-20 03:00:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,4 +4,11 @@ def reverse_each_word(str)
     .map { |spacer1, word, spacer2| spacer1 + word.reverse + spacer2 }
     .join
 end
+
+def reverse_each_word(str)
+  str
+    .scan(/(\s*)(\S+)(\s*)/)
+    .collect { |spacer1, word, spacer2| spacer1 + word.reverse + spacer2 }
+    .join
+end
   
