67774b97c6670aea4ce8a57d11220395a00b1536
Paul Ralyea <047par@gmail.com>
2020-03-20 02:54:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,10 @@
 def reverse_each_word(sentence)
-  sentence.split.each {|i| i.reverse}.join(" ")
+ # sentence.split.each {|i| i.reverse}.join(" ")
+#end
+
+def reverse_words(str)
+  str
+    #.scan(/(\s*)(\S+)(\s*)/)
+    .each { |spacer1, word, spacer2| spacer1 + word.reverse + spacer2 }
+    .join
 end
